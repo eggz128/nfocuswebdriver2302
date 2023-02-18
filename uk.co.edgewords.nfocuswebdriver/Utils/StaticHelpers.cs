29efa3e6cc565c9746bb01e1a89db3474e73d665
Steve Powell <steve.powell@edgewords.co.uk>
2023-02-18 14:30:27 +0000

    Added how to get an element size and location

--- uk.co.edgewords.nfocuswebdriver/Utils/StaticHelpers.cs
+++ uk.co.edgewords.nfocuswebdriver/Utils/StaticHelpers.cs
@@ -19,12 +19,21 @@ namespace uk.co.edgewords.nfocuswebdriver.Utils
             wait2.Until(drv => drv.FindElement(locator).Displayed);
         }
 
+        //In sel v4 we can take screenshots of elements - not just the whole page.
         public static void TakeScreenshotOfElement(IWebDriver driver, By locator, string filename)
         {
             IWebElement form = driver.FindElement(locator);
+            
+            //In sel v3 we might have to find the position and size of an element
+            //before then cutting it out of the full page screenshot with some other image manip library
+            //but not now... might still be ueful to know size/location at times though
+            //int xFromLeft = form.Location.X;
+            //int width = form.Size.Width;
+
+
             ITakesScreenshot formss = form as ITakesScreenshot;
             var screenshotForm = formss.GetScreenshot();
-            screenshotForm.SaveAsFile(@"D:\screenshots\" + filename, ScreenshotImageFormat.Png);
+            screenshotForm.SaveAsFile(@"D:\screenshots\" + filename, ScreenshotImageFormat.Png); //ToDo: Timstamp screenshots so they are not overwritten
             TestContext.WriteLine("Screenshot taken - see report");
             TestContext.AddTestAttachment(@"D:\screenshots\" + filename);
         }
