4c31e8f76b21576a7c97a1b6539c0d164e1b7ec6
Steve Powell <steve.powell@edgewords.co.uk>
2023-02-18 14:32:08 +0000

    According to c# naming conventions, private fields should be underscored e.g. _nameOfField

--- uk.co.edgewords.nfocuswebdriver/Utils/MyHelpers.cs
+++ uk.co.edgewords.nfocuswebdriver/Utils/MyHelpers.cs
@@ -13,17 +13,17 @@ namespace uk.co.edgewords.nfocuswebdriver.Utils
         //This is an instance class - it will nee to be instantiated (turned in to an object) before use
 
         //Methods need access to a WebDriver instance. They can use this field, although an actual web driver will have to be placed in it first...
-        private IWebDriver driver;
+        private IWebDriver _driver;
         //...which can be done via the constructor
         //When the class is instantiated have the driver passed in
         public MyHelpers(IWebDriver driver)
         {
-            this.driver = driver;
+            this._driver = driver;
         }
         //A generic helper method that can be reused by any tests that need it
         public void WaitForElement(By locator, int timeToWaitInSeconds)
         {
-            WebDriverWait wait2 = new WebDriverWait(driver, TimeSpan.FromSeconds(timeToWaitInSeconds));
+            WebDriverWait wait2 = new WebDriverWait(_driver, TimeSpan.FromSeconds(timeToWaitInSeconds));
             wait2.Until(drv => drv.FindElement(locator).Enabled);
         }
     }
