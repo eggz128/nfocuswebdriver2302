5637b5e3c665b25900447bb0151ee48b3b71a71a
Steve Powell <steve.powell@edgewords.co.uk>
2023-02-18 14:29:54 +0000

    Added more comments and how to maximise a browser.

--- uk.co.edgewords.nfocuswebdriver/TestCases/SelIDEDemoTest.cs
+++ uk.co.edgewords.nfocuswebdriver/TestCases/SelIDEDemoTest.cs
@@ -16,9 +16,14 @@ using NUnit.Framework;
 namespace uk.co.edgewords.nfocuswebdriver.TestCases
 {
     //[TestFixture] //Optional annotation on a class that contains tests. NUnit does not need this.
-    public class SelIDEDemoTest : Utils.BaseTest
+    public class SelIDEDemoTest : Utils.BaseTest //Have this test class inherit setup, teardown and driver from a base class
     {
-        
+        //SelIDE [SetUp] and [TearDown] removed
+        //They include some unncessary code, like setting up things for
+        //javascript injection in to the browser
+        //Why might you want to do that? Maybe change page styles to highlight an element before screenshots.
+
+
         [Test]
         public void selIDEDemo()
         {
@@ -28,7 +33,17 @@ namespace uk.co.edgewords.nfocuswebdriver.TestCases
             // 1 | open | /webdriver2/ | 
             driver.Navigate().GoToUrl("https://www.edgewordstraining.co.uk/webdriver2/");
             // 2 | setWindowSize | 974x1032 | 
+            //sets a particular window size - probably something you want to do especially if executing headless (see base class)
             driver.Manage().Window.Size = new System.Drawing.Size(974, 1032);
+
+            //driver.Manage().Window.Maximize(); //Or just maximise the window
+            
+            //Remember many pages will have alternative rendering styles depending on screen size.
+            //This might go as far as also changing /how/ to interact with a page
+            //e.g. instead of a full navbar across the top, maybe navigation gets collapsed in to a hamburger menu structure on mobile
+            //In that case you would probably need seperate tests
+            //and might also consider Appium - mobile testing based on WebDriver - https://appium.io/
+
             // 3 | click | css=.last span | 
             driver.FindElement(By.CssSelector(".last span")).Click();
             // 4 | click | linkText=Forms | 
@@ -56,6 +71,7 @@ namespace uk.co.edgewords.nfocuswebdriver.TestCases
             driver.FindElement(By.CssSelector("tr:nth-child(4) > td:nth-child(3)")).Click();
             // 14 | verifyText | id=selectValue | Selection One
 
+            //Dont stop the test if assertion fails - but still report a fail ultimately
             try
             {
                 Assert.That(driver.FindElement(By.Id("selectValue")).Text, Is.EqualTo("Selection Two"));
@@ -63,12 +79,16 @@ namespace uk.co.edgewords.nfocuswebdriver.TestCases
             catch (Exception)
             {
                 Console.WriteLine("Got wrong selection");
-                //throw;
+                //throw; //if you throw test will stop
             }
 
 
             // 15 | click | linkText=CSS/XPath | 
             driver.FindElement(By.LinkText("CSS/XPath")).Click();
+            
+            
+            //Unfortunately SelIDE isn't perfect. These drag and drop actions don't work as exported
+            //and would have to be rewritten.
             // 16 | mouseDownAt | css=.ui-slider-handle | 8.512481689453125,6.86248779296875
             {
                 var element = driver.FindElement(By.CssSelector(".ui-slider-handle"));
